c719fed8d76f97c38cf88aa8e2e7774e8654aca5
iGROoT9i <jonnymlv9@gmail.com>
2026-06-22 14:33:55 -0500

    first commit 8

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,5 @@
 import { motion } from 'framer-motion';
-import { Cross, MapPin, Mail, Phone, Heart, Smartphone, Building } from 'lucide-react';
+import { Cross, MapPin, Mail, Phone, Heart, Smartphone } from 'lucide-react';
 
 export default function Footer() {
   return (
